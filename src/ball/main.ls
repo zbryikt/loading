3f953759100d027fe694e51d182ef99e982e7a69
Kirby T. Wu <tkirby@gmail.com>
2014-04-16 10:26:52 +0800

    add a variant to ball

--- src/ball/main.ls
+++ src/ball/main.ls
@@ -5,20 +5,32 @@ angular.module \uiloading
     ret = do
       mode: \both
       radius: 15
+      variant: 1
       vars: 
+        * name: 'variant', placeholder: '', type: 'choice', values: <[jump rotate rubberband]>, attr: 'variant'
         * name: 'color', placeholder: '#f00', type: 'color', default: '#000', attr: 'color'
         * name: 'radius', placeholder: '15', type: 'px', default: '15', attr: 'radius'
       patch-css: (data, opt) -> @patch data, opt
       patch-svg: (data, opt) -> @patch data, opt
       patch: (data, opt) ->
-        data = data.replace /#000|black/g, "#{opt.c1}"
+        data = data.replace /uil-ball-anim-type/g, "uil-ball-anim#{@variant}"
+        data = data.replace /#000|black/g, "#{opt.c2}"
         data = data.replace /1s/g, "#{opt.speed}s"
-        data = data.replace /15/g, "#{opt.c2}"
-        data = data.replace /ballRadius2/g, "#{4 * opt.c2}px"
-        data = data.replace /ballRadius/g, "#{2 * opt.c2}px"
+        data = data.replace /15/g, "#{opt.c3}"
+        data = data.replace /ballRadius2/g, "#{4 * opt.c3}px"
+        data = data.replace /ballRadius/g, "#{2 * opt.c3}px"
         data = data.replace /svg width="100%" height="100%"/, "svg width='#{opt.size * 2}px' height='#{opt.size * 2}px'"
         data = data.replace /"uil-ball-css"/, "'uil-ball-css' style='-webkit-transform:scale(#{opt.size * 2 / 200})'"
       custom: (s, e, a, c) ->
+        a.$observe 'variant' (v) ~>
+          @variant = switch v
+          | \jump => 0
+          | \rotate => 1
+          | \rubberband => 2
+          e.find \circle
+            .attr \transform, if @variant == 1 => "translate(30 0)" else "translate(0 0)"
+            .attr \cy, 0
+          e.find "g > g" .attr \transform, "rotate(0)"
         a.$observe 'color' (v) -> if v => e.find \circle .css \fill, v
         a.$observe 'radius' (v) ~> if v => 
           @radius = v
@@ -27,6 +39,12 @@ angular.module \uiloading
       start: (s, e, a, c) ->
       stop: (s, e, a, c) ->
       step: (s, e, a, c, delay) ->
-        e.find \circle .each ->
+        switch @variant
+        | 0 =>
+          <- e.find \circle .each
           v = ( (500 - ( delay % 1000 )) / 500 ) ** 2
-          $(&1)attr \cy, "#{20 + v * 60}"
+          $(&1)attr \cy, "#{-30 + v * 60}"
+        | 1 =>
+          <- e.find "g > g" .each
+          v = 360 * ( ( delay % 1000 ) / 1000 )
+          $(&1)attr \transform, "rotate(#{v})"
--- static/js/ball.js
+++ static/js/ball.js
@@ -7,8 +7,15 @@ define([], function(){
     return ret = {
       mode: 'both',
       radius: 15,
+      variant: 1,
       vars: [
         {
+          name: 'variant',
+          placeholder: '',
+          type: 'choice',
+          values: ['jump', 'rotate', 'rubberband'],
+          attr: 'variant'
+        }, {
           name: 'color',
           placeholder: '#f00',
           type: 'color',
@@ -29,16 +36,31 @@ define([], function(){
         return this.patch(data, opt);
       },
       patch: function(data, opt){
-        data = data.replace(/#000|black/g, opt.c1 + "");
+        data = data.replace(/uil-ball-anim-type/g, "uil-ball-anim" + this.variant);
+        data = data.replace(/#000|black/g, opt.c2 + "");
         data = data.replace(/1s/g, opt.speed + "s");
-        data = data.replace(/15/g, opt.c2 + "");
-        data = data.replace(/ballRadius2/g, 4 * opt.c2 + "px");
-        data = data.replace(/ballRadius/g, 2 * opt.c2 + "px");
+        data = data.replace(/15/g, opt.c3 + "");
+        data = data.replace(/ballRadius2/g, 4 * opt.c3 + "px");
+        data = data.replace(/ballRadius/g, 2 * opt.c3 + "px");
         data = data.replace(/svg width="100%" height="100%"/, "svg width='" + opt.size * 2 + "px' height='" + opt.size * 2 + "px'");
         return data = data.replace(/"uil-ball-css"/, "'uil-ball-css' style='-webkit-transform:scale(" + opt.size * 2 / 200 + ")'");
       },
       custom: function(s, e, a, c){
         var this$ = this;
+        a.$observe('variant', function(v){
+          this$.variant = (function(){
+            switch (v) {
+            case 'jump':
+              return 0;
+            case 'rotate':
+              return 1;
+            case 'rubberband':
+              return 2;
+            }
+          }());
+          e.find('circle').attr('transform', this$.variant === 1 ? "translate(30 0)" : "translate(0 0)").attr('cy', 0);
+          return e.find("g > g").attr('transform', "rotate(0)");
+        });
         a.$observe('color', function(v){
           if (v) {
             return e.find('circle').css('fill', v);
@@ -59,11 +81,20 @@ define([], function(){
       start: function(s, e, a, c){},
       stop: function(s, e, a, c){},
       step: function(s, e, a, c, delay){
-        return e.find('circle').each(function(){
-          var v;
-          v = Math.pow((500 - delay % 1000) / 500, 2);
-          return $(arguments[1]).attr('cy', (20 + v * 60) + "");
-        });
+        switch (this.variant) {
+        case 0:
+          return e.find('circle').each(function(){
+            var v;
+            v = Math.pow((500 - delay % 1000) / 500, 2);
+            return $(arguments[1]).attr('cy', (-30 + v * 60) + "");
+          });
+        case 1:
+          return e.find("g > g").each(function(){
+            var v;
+            v = 360 * ((delay % 1000) / 1000);
+            return $(arguments[1]).attr('transform', "rotate(" + v + ")");
+          });
+        }
       }
     };
   });
