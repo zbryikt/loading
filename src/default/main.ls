5ce55483d1b0a6674dffa04b3fa2f6bd025ec5b8
Kirby T. Wu <tkirby@gmail.com>
2014-04-27 16:57:37 +0800

    upgrade default to high configurability

--- src/default/main.ls
+++ src/default/main.ls
@@ -1,31 +1,92 @@
 <- define <[]>
 angular.module \uiloading
   ..factory \uilType-default, ($interval, uilresize) ->
-    offset = [0 83 166 250 333 416 500 583 666 750 833 916 1000]
     start = null
     ret = do
       mode: \both
+      count: 12
+      width: 7
+      height: 20
+      radius: 5
+      offset: 30
+      color: \#00b2ff
+      speed: 1
       vars: 
-        [ { name: 'bar color', placeholder: '#000', type: 'color', default: '#02bde7', attr: 'bar-color' } ]
-      patch-css: (data, opt) -> @patch data, opt
-      patch-svg: (data, opt) -> @patch data, opt
-      patch: (data, opt) ->
-        data = data.replace /barColor/g, opt.c1
-        data = data.replace /bkColor/g, opt.cbk
-        data = data.replace /duration/g, "#{opt.speed}s"
-        data = uilresize data, \default, opt
-        # always do this reversely so begin1 won't override begin12
-        for i from 12 to 1 by -1 =>
-          begin = "#{parse-int(((i - 1) * opt.speed / 12) * 100) / 100 }s"
-          data = data.replace(new RegExp("begin#i", "g"), begin)
-        data
+        * name: 'color', placeholder: '#000', type: 'color', default: '#00b2ff', attr: 'color'
+        * name: 'count', placeholder: '12', type: 'px', default: '12', attr: 'count'
+        * name: 'width', placeholder: '7', type: 'px', default: '7', attr: 'width'
+        * name: 'height', placeholder: '20', type: 'px', default: '20', attr: 'height'
+        * name: 'radius', placeholder: '5', type: 'px', default: '5', attr: 'radius'
+        * name: 'offset', placeholder: '30', type: 'px', default: '30', attr: 'offset'
+      patch-svg: (data, opt) -> 
+        svg-head = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" '+
+        'viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-default">'+
+        '<rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect>'
+        svg = ""
+        for i from 0 til @count =>
+          svg += "<rect " +
+          " x='#{50 - @width/2}' y='#{50 - @height/2}'" +
+          " width='#{@width}' height='#{@height}'" +
+          " rx='#{@radius}' ry='#{@radius}'" +
+          " fill='#{@color}'" +
+          " transform='rotate(#{360 * i / @count} 50 50) translate(0 -#{@offset})'>"+
+          "  <animate attributeName='opacity' from='1' to='0' dur='#{opt.speed}s'"+
+          " begin='#{i * opt.speed / @count}s' repeatCount='indefinite'/>"+
+          "</rect>"
+        svg = svg-head + svg + "</svg>"
+        svg = uilresize svg, \default, opt
+
+      patch-css: (data, opt) ->
+        [html,css] = ["",""]
+        keyframes = "{ 0% { opacity: 1} 100% {opacity: 0} }"
+        speed = opt.speed
+        animation-value = "uil-default-anim #{speed}s linear infinite"
+        css = "@-webkit-keyframes uil-default-anim #{keyframes}"+
+        "@keyframes uil-default-anim #{keyframes}"
+        for i from 0 til @count 
+          transform = "rotate(#{parseInt(360 * i / @count)}deg) translate(0,-#{@offset * 2}px)"
+          style = "top:#{100 - @height}px;left:#{100 - @width}px;width:#{@width * 2}px;height:#{@height * 2}px;" +
+            "background:#{@color};-webkit-transform:#{transform};transform:#{transform};" +
+            "border-radius:#{@radius * 2}px;"
+          one-css = ".uil-default-css > div:nth-of-type(#{i+1}){"+
+          "-webkit-animation: #{animation-value};"+
+          "animation: #{animation-value};"+
+          "-webkit-animation-delay: #{i*speed / @count - speed / 2}s;"+
+          "animation-delay: #{i*speed / @count - speed / 2}s;"+
+          "}"
+          css += one-css
+          html += "<div style='#{style}'></div>"
+        html = "<style type='text/css'>#{css}</style><div class=\"uil-default-css\">#{html}</div>"
+        html = uilresize html, \default, opt
+
+      render: (s, e, a, c, opt) ->
+        @ <<< opt
+        svg = d3.select(e.0)select \svg
+        svg.selectAll(\rect.bar).data [(i * 360 / @count) for i from 0 til @count]
+          ..exit!remove!
+          ..enter!append \rect .attr \class, \bar
+        svg.selectAll \rect.bar
+          .attr do
+            x: "#{50 - @width/2}"
+            y: "#{50 - @height/2}"
+            width: @width
+            height: @height
+            fill: @color
+            rx: @radius
+            ry: @radius
+            transform: (d,i) ~> "rotate(#{360 * i / @count} 50 50) translate(0 -#{@offset})"
       custom: (s, e, a, c) ->
-        a.$observe 'barColor' (v) -> if v => e.find \rect.bar .css \fill, v
-        a.$observe 'background' (v) -> if v => e.find \rect.bk .css \fill, v
+        a.$observe 'color' (v) ~> if v => @render s,e,a,c,color: v
+        a.$observe 'count' (v) ~> if v => @render s,e,a,c,count: v
+        a.$observe 'width' (v) ~> if v => @render s,e,a,c,width: v
+        a.$observe 'height' (v) ~> if v => @render s,e,a,c,height: v
+        a.$observe 'radius' (v) ~> if v => @render s,e,a,c,radius: v
+        a.$observe 'offset' (v) ~> if v => @render s,e,a,c,offset: v
+
       start: (s, e, a, c) ->
       stop: (s, e, a, c) ->
-        if s.timer => $interval.cancel s.timer
       step: (s, e, a, c, delay) ->
-        e.find "rect.bar" .each ->
-          delta = ( delay + offset[12 - &0] ) % 1000
-          $(&1)css \opacity, delta / 1000
+        v = ( delay % 1000 ) / 1000
+        e.find \rect.bar .each ~>
+          v = ( ( delay + ((@count - &0 - 1) * 1000 / @count) ) % 1000 ) / 1000
+          $(&1)attr \opacity, v
--- static/js/default.js
+++ static/js/default.js
@@ -2,63 +2,174 @@ define([], function(){
   var x$;
   x$ = angular.module('uiloading');
   x$.factory('uilType-default', function($interval, uilresize){
-    var offset, start, ret;
-    offset = [0, 83, 166, 250, 333, 416, 500, 583, 666, 750, 833, 916, 1000];
+    var start, ret;
     start = null;
     return ret = {
       mode: 'both',
-      vars: [{
-        name: 'bar color',
-        placeholder: '#000',
-        type: 'color',
-        'default': '#02bde7',
-        attr: 'bar-color'
-      }],
-      patchCss: function(data, opt){
-        return this.patch(data, opt);
-      },
-      patchSvg: function(data, opt){
-        return this.patch(data, opt);
-      },
-      patch: function(data, opt){
-        var i$, i, begin;
-        data = data.replace(/barColor/g, opt.c1);
-        data = data.replace(/bkColor/g, opt.cbk);
-        data = data.replace(/duration/g, opt.speed + "s");
-        data = uilresize(data, 'default', opt);
-        for (i$ = 12; i$ >= 1; --i$) {
-          i = i$;
-          begin = parseInt(((i - 1) * opt.speed / 12) * 100) / 100 + "s";
-          data = data.replace(new RegExp("begin" + i, "g"), begin);
+      count: 12,
+      width: 7,
+      height: 20,
+      radius: 5,
+      offset: 30,
+      color: '#00b2ff',
+      speed: 1,
+      vars: [
+        {
+          name: 'color',
+          placeholder: '#000',
+          type: 'color',
+          'default': '#00b2ff',
+          attr: 'color'
+        }, {
+          name: 'count',
+          placeholder: '12',
+          type: 'px',
+          'default': '12',
+          attr: 'count'
+        }, {
+          name: 'width',
+          placeholder: '7',
+          type: 'px',
+          'default': '7',
+          attr: 'width'
+        }, {
+          name: 'height',
+          placeholder: '20',
+          type: 'px',
+          'default': '20',
+          attr: 'height'
+        }, {
+          name: 'radius',
+          placeholder: '5',
+          type: 'px',
+          'default': '5',
+          attr: 'radius'
+        }, {
+          name: 'offset',
+          placeholder: '30',
+          type: 'px',
+          'default': '30',
+          attr: 'offset'
         }
-        return data;
+      ],
+      patchSvg: function(data, opt){
+        var svgHead, svg, i$, to$, i;
+        svgHead = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" ' + 'viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-default">' + '<rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect>';
+        svg = "";
+        for (i$ = 0, to$ = this.count; i$ < to$; ++i$) {
+          i = i$;
+          svg += "<rect " + (" x='" + (50 - this.width / 2) + "' y='" + (50 - this.height / 2) + "'") + (" width='" + this.width + "' height='" + this.height + "'") + (" rx='" + this.radius + "' ry='" + this.radius + "'") + (" fill='" + this.color + "'") + (" transform='rotate(" + 360 * i / this.count + " 50 50) translate(0 -" + this.offset + ")'>") + ("  <animate attributeName='opacity' from='1' to='0' dur='" + opt.speed + "s'") + (" begin='" + i * opt.speed / this.count + "s' repeatCount='indefinite'/>") + "</rect>";
+        }
+        svg = svgHead + svg + "</svg>";
+        return svg = uilresize(svg, 'default', opt);
       },
-      custom: function(s, e, a, c){
-        a.$observe('barColor', function(v){
-          if (v) {
-            return e.find('rect.bar').css('fill', v);
+      patchCss: function(data, opt){
+        var ref$, html, css, keyframes, speed, animationValue, i$, to$, i, transform, style, oneCss;
+        ref$ = ["", ""], html = ref$[0], css = ref$[1];
+        keyframes = "{ 0% { opacity: 1} 100% {opacity: 0} }";
+        speed = opt.speed;
+        animationValue = "uil-default-anim " + speed + "s linear infinite";
+        css = ("@-webkit-keyframes uil-default-anim " + keyframes) + ("@keyframes uil-default-anim " + keyframes);
+        for (i$ = 0, to$ = this.count; i$ < to$; ++i$) {
+          i = i$;
+          transform = "rotate(" + parseInt(360 * i / this.count) + "deg) translate(0,-" + this.offset * 2 + "px)";
+          style = ("top:" + (100 - this.height) + "px;left:" + (100 - this.width) + "px;width:" + this.width * 2 + "px;height:" + this.height * 2 + "px;") + ("background:" + this.color + ";-webkit-transform:" + transform + ";transform:" + transform + ";") + ("border-radius:" + this.radius * 2 + "px;");
+          oneCss = (".uil-default-css > div:nth-of-type(" + (i + 1) + "){") + ("-webkit-animation: " + animationValue + ";") + ("animation: " + animationValue + ";") + ("-webkit-animation-delay: " + (i * speed / this.count - speed / 2) + "s;") + ("animation-delay: " + (i * speed / this.count - speed / 2) + "s;") + "}";
+          css += oneCss;
+          html += "<div style='" + style + "'></div>";
+        }
+        html = "<style type='text/css'>" + css + "</style><div class=\"uil-default-css\">" + html + "</div>";
+        return html = uilresize(html, 'default', opt);
+      },
+      render: function(s, e, a, c, opt){
+        var svg, x$, i, this$ = this;
+        import$(this, opt);
+        svg = d3.select(e[0]).select('svg');
+        x$ = svg.selectAll('rect.bar').data((function(){
+          var i$, to$, results$ = [];
+          for (i$ = 0, to$ = this.count; i$ < to$; ++i$) {
+            i = i$;
+            results$.push(i * 360 / this.count);
+          }
+          return results$;
+        }.call(this)));
+        x$.exit().remove();
+        x$.enter().append('rect').attr('class', 'bar');
+        return svg.selectAll('rect.bar').attr({
+          x: (50 - this.width / 2) + "",
+          y: (50 - this.height / 2) + "",
+          width: this.width,
+          height: this.height,
+          fill: this.color,
+          rx: this.radius,
+          ry: this.radius,
+          transform: function(d, i){
+            return "rotate(" + 360 * i / this$.count + " 50 50) translate(0 -" + this$.offset + ")";
           }
         });
-        return a.$observe('background', function(v){
+      },
+      custom: function(s, e, a, c){
+        var this$ = this;
+        a.$observe('color', function(v){
           if (v) {
-            return e.find('rect.bk').css('fill', v);
+            return this$.render(s, e, a, c, {
+              color: v
+            });
+          }
+        });
+        a.$observe('count', function(v){
+          if (v) {
+            return this$.render(s, e, a, c, {
+              count: v
+            });
+          }
+        });
+        a.$observe('width', function(v){
+          if (v) {
+            return this$.render(s, e, a, c, {
+              width: v
+            });
+          }
+        });
+        a.$observe('height', function(v){
+          if (v) {
+            return this$.render(s, e, a, c, {
+              height: v
+            });
+          }
+        });
+        a.$observe('radius', function(v){
+          if (v) {
+            return this$.render(s, e, a, c, {
+              radius: v
+            });
+          }
+        });
+        return a.$observe('offset', function(v){
+          if (v) {
+            return this$.render(s, e, a, c, {
+              offset: v
+            });
           }
         });
       },
       start: function(s, e, a, c){},
-      stop: function(s, e, a, c){
-        if (s.timer) {
-          return $interval.cancel(s.timer);
-        }
-      },
+      stop: function(s, e, a, c){},
       step: function(s, e, a, c, delay){
-        return e.find("rect.bar").each(function(){
-          var delta;
-          delta = (delay + offset[12 - arguments[0]]) % 1000;
-          return $(arguments[1]).css('opacity', delta / 1000);
+        var v, this$ = this;
+        v = (delay % 1000) / 1000;
+        return e.find('rect.bar').each(function(){
+          var v;
+          v = ((delay + (this$.count - arguments[0] - 1) * 1000 / this$.count) % 1000) / 1000;
+          return $(arguments[1]).attr('opacity', v);
         });
       }
     };
   });
   return x$;
 });
+function import$(obj, src){
+  var own = {}.hasOwnProperty;
+  for (var key in src) if (own.call(src, key)) obj[key] = src[key];
+  return obj;
+}
